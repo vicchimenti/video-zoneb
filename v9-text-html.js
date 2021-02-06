b2ce13902902bdee5a65bd8b909a6b1aaf629835
vicchimenti <victor.chimenti@gmail.com>
2021-02-06 13:59:37 -0800

    add js v

---
+++ v9-text-html.js
@@ -0,0 +1,27 @@
+var fieldToBeEvaluated = com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, '<t4 type="content" name="Category" output="normal" display_field="value" delimiter=";" />');
+var optionToTestFor = "socialJustice"; //edit this to change the option
+var contentTypeLayout = 'output/gridfeed'; //edit this to change the Content Layout to use for output
+var n = fieldToBeEvaluated.indexOf(optionToTestFor); /* determines starting character of string */
+
+
+/***
+ *  send correct layout to the document
+ * 
+ */
+try {
+
+    /* if content exists, it'll start at 0 or later, so process this */
+    if ((n >= 0)) {
+        var sw = new java.io.StringWriter();
+        var t4w = new com.terminalfour.utils.T4StreamWriter(sw);
+        new com.terminalfour.publish.ContentPublisher().write(t4w, dbStatement, publishCache, section, content, contentTypeLayout, isPreview);
+        output = sw.toString();
+
+        // write to page document
+        document.write(output);
+    }
+
+
+} catch (err) {
+    document.write(err.message);
+}
