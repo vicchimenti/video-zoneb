5297034d33d1b1e09b3d13bd2d0f1150d41f21f3
vicchimenti <victor.chimenti@gmail.com>
2021-02-07 09:27:27 -0800

    declare href and src

--- text-bzone.js
+++ text-bzone.js
@@ -36,6 +36,8 @@ try {
      *  Declare/Assign local variables with base formatting
      * 
      * */
+    href= "";
+    src = "";
     var videoPlayerString =     "<iframe title='videoName' src='<t4 type=&quot;content&quot; name=&quot;Video Type&quot; output=&quot;normal&quot; display_field=&quot;value&quot; /> \
                                 <t4 type=&quot;content&quot; name=&quot;vimeoID&quot; output=&quot;normal&quot; modifiers=&quot;striptags&quot; />?color=aa0000&title=0&byline=0&portrait=0&rel=0&showinfo=0' \
                                 width='585' height='329' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
